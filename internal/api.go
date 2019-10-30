ebd27307875626eb9ba30b35b2b882b5c8385669
Alex Baily <alex.baily@ensono.com>
2019-10-30 15:19:05 +0000

    Add GetIntFromIP to parse IP address into an int.

--- internal/api.go
+++ internal/api.go
@@ -1,21 +1,40 @@
 package internal
 
+import (
+	"strconv"
+	"strings"
+)
+
+//NetCache will instantiate a singleton map for storing ranges in a cache.
+var NetCache = make(map[string][]string)
+
 type SecurityGroup struct {
-    name string
-    rules[] SecurityGroupRule
+	name  string
+	rules []SecurityGroupRule
 }
 
 type SecurityGroupRule struct {
-    ports string
-    networks[] NetRange
+	ports    string
+	networks []NetRange
 }
-
 
 type NetRange struct {
-    cidr string
-    range[]
+	cidr         string
+	networkRange []string
 }
 
+func GetIntFromIP(cidrRange string) (i int64) {
+	cidr := strings.Split(cidrRange, ".")
 
-//This "cache" will be 
-type NetCache map[string]string[]
+	quad1, _ := strconv.ParseInt(cidr[0], 10, 64)
+	quad2, _ := strconv.ParseInt(cidr[1], 10, 64)
+	quad3, _ := strconv.ParseInt(cidr[2], 10, 64)
+	quad4, _ := strconv.ParseInt(cidr[3], 10, 64)
+
+	return int64(quad1<<24 + quad2<<16 + quad3<<8 + quad4)
+}
+
+/*
+func CheckRange(cidr string) (n NetRange, b bool) {
+
+}*/
---
+++ internal/file_parse.go
@@ -0,0 +1,11 @@
+package internal
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func GetSecurityGroupRules(file string) {
+
+}
